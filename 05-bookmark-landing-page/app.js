98f58b1f8d4841354deadf5d4131c46afd73885e
Svetlana Stoycheva <s.stoycheva@gmail.com>
2021-09-08 16:07:55 +0300

    project 05, Features - JS completed

--- 05-bookmark-landing-page/app.js
+++ 05-bookmark-landing-page/app.js
@@ -13,3 +13,52 @@ closeBtn.addEventListener('click', () => {
   sidebar.classList.remove('show-sidebar');
   navbarSmallScreen.classList.remove('hide-small-navbar');
 });
+
+// Features functionality
+const featuresBtns = document.querySelectorAll('.features-button');
+const container = document.querySelector('.featires-container');
+
+const imageEl = document.querySelector('.features-photo');
+const title = document.querySelector('.features-title');
+const text = document.querySelector('.features-text');
+
+const features = [
+  {
+    title: 'Bookmark in one click',
+    text:
+      'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.',
+    image: './images/illustration-features-tab-1.svg',
+  },
+  {
+    title: 'Intelligent search',
+    text:
+      'Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.',
+    image: './images/illustration-features-tab-2.svg',
+  },
+  {
+    title: 'Share your bookmarks',
+    text:
+      'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button. ',
+    image: './images/illustration-features-tab-3.svg',
+  },
+];
+
+featuresBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    btn.classList.add('features-button-active');
+    
+    if (e.target.classList.contains('first')) {
+      text.textContent = features[0].text;
+      title.textContent = features[0].title;
+      imageEl.src = features[0].image;
+    } else if (e.target.classList.contains('second')) {
+      text.textContent = features[1].text;
+      title.textContent = features[1].title;
+      imageEl.src = features[1].image;
+    } else if (e.target.classList.contains('third')) {
+      text.textContent = features[2].text;
+      title.textContent = features[2].title;
+      imageEl.src = features[2].image;
+    }
+  });
+});
